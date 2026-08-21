9b451910018af8aa510ec4e71aec57461fa95c99
Deiby Mejia Ruiz <47683800+elmaedesistemas@users.noreply.github.com>
2026-08-20 23:18:14 -0600

    feat(frontend): manage file data with Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,78 +1,105 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import {
-  Alert,
-  Container,
-  Navbar,
-  Spinner
+    Alert,
+    Container,
+    Navbar,
+    Spinner
 } from 'react-bootstrap'
+import {
+    useDispatch,
+    useSelector
+} from 'react-redux'
 import FileFilter from './components/FileFilter'
 import FilesTable from './components/FilesTable'
-import useFilesData from './hooks/useFilesData'
-import useFilesList from './hooks/useFilesList'
+import {
+    loadFilesData,
+    loadFilesList,
+    setSelectedFileName
+} from './store/filesSlice'
 import './styles.css'
 
-function App () {
-  const [selectedFileName, setSelectedFileName] = useState('')
+function App() {
+    const dispatch = useDispatch()
 
-  const {
-    fileNames,
-    isLoading: isListLoading,
-    error: listError
-  } = useFilesList()
+    const {
+        files,
+        fileNames,
+        selectedFileName,
+        isDataLoading,
+        isListLoading,
+        dataError,
+        listError
+    } = useSelector(state => state.files)
 
-  const {
-    files,
-    isLoading: isDataLoading,
-    error: dataError
-  } = useFilesData(selectedFileName)
+    useEffect(() => {
+        const request = dispatch(loadFilesList())
 
-  return (
-    <>
-      <Navbar bg='danger' variant='dark'>
-        <Container fluid>
-          <Navbar.Brand>React Test App</Navbar.Brand>
-        </Container>
-      </Navbar>
+        return () => {
+            request.abort()
+        }
+    }, [dispatch])
 
-      <Container fluid className='page-container'>
-        <h1 className='h3 mb-4'>Files data</h1>
+    useEffect(() => {
+        const request = dispatch(
+            loadFilesData(selectedFileName)
+        )
 
-        <FileFilter
-          fileNames={fileNames}
-          selectedFileName={selectedFileName}
-          isLoading={isListLoading}
-          onChange={setSelectedFileName}
-        />
+        return () => {
+            request.abort()
+        }
+    }, [dispatch, selectedFileName])
 
-        {listError && (
-          <Alert variant='warning'>
-            The file filter is currently unavailable.
-          </Alert>
-        )}
+    function handleFileChange(fileName) {
+        dispatch(setSelectedFileName(fileName))
+    }
 
-        {isDataLoading && (
-          <div
-            className='loading-container'
-            role='status'
-            aria-live='polite'
-          >
-            <Spinner animation='border' variant='danger' />
-            <span>Loading files...</span>
-          </div>
-        )}
+    return (
+        <>
+            <Navbar bg='danger' variant='dark'>
+                <Container fluid>
+                    <Navbar.Brand>React Test App</Navbar.Brand>
+                </Container>
+            </Navbar>
 
-        {!isDataLoading && dataError && (
-          <Alert variant='danger'>
-            {dataError}
-          </Alert>
-        )}
+            <Container fluid className='page-container'>
+                <h1 className='h3 mb-4'>Files data</h1>
 
-        {!isDataLoading && !dataError && (
-          <FilesTable files={files} />
-        )}
-      </Container>
-    </>
-  )
+                <FileFilter
+                    fileNames={fileNames}
+                    selectedFileName={selectedFileName}
+                    isLoading={isListLoading}
+                    onChange={handleFileChange}
+                />
+
+                {listError && (
+                    <Alert variant='warning'>
+                        The file filter is currently unavailable.
+                    </Alert>
+                )}
+
+                {isDataLoading && (
+                    <div
+                        className='loading-container'
+                        role='status'
+                        aria-live='polite'
+                    >
+                        <Spinner animation='border' variant='danger' />
+                        <span>Loading files...</span>
+                    </div>
+                )}
+
+                {!isDataLoading && dataError && (
+                    <Alert variant='danger'>
+                        {dataError}
+                    </Alert>
+                )}
+
+                {!isDataLoading && !dataError && (
+                    <FilesTable files={files} />
+                )}
+            </Container>
+        </>
+    )
 }
 
 export default App
